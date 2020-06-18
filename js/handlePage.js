83f85e1f9c0728a5f0b30492e4cc131a6cc38d7e
John Peter <jay3pee@gmail.com>
2020-06-18 23:20:52 +0100

    feat:added the ministers's handle section

---
+++ js/handlePage.js
@@ -0,0 +1,34 @@
+let ministerChevDown = document.querySelector("#minister-chev-down");
+let ministerChevUp = document.querySelector("#minister-chev-up");
+let ministryChevDown = document.querySelector("#ministry-chev-down");
+let ministryChevUp = document.querySelector("#ministry-chev-up");
+let ministerTab = document.querySelector("#minister-tab");
+let ministryTab = document.querySelector("#ministry-tab");
+let ministryToggle = document.querySelector("#ministry-toggle");
+let ministerToggle = document.querySelector("#minister-toggle");
+
+// ministerChev.addEventListener("click", e => {
+//   console.log(ministerChev);
+//   if ((ministerVisibility = false)) {
+//     console.log(ministerTab);
+//     ministerTab.style.display = "block";
+//     ministerVisibility = true;
+//   } else {
+//     ministerTab.style.display = "none";
+//     ministerVisibility = false;
+//   }
+// });
+
+function showMinister() {
+  ministerToggle.classList.toggle("hovered");
+  ministerTab.classList.toggle("visible");
+  ministerChevDown.classList.toggle("invisible");
+  ministerChevUp.classList.toggle("visible");
+}
+
+function showMinistry() {
+  ministryToggle.classList.toggle("hovered");
+  ministryTab.classList.toggle("visible");
+  ministryChevDown.classList.toggle("invisible");
+  ministryChevUp.classList.toggle("visible");
+}

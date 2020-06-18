0d2af4f6438c5ee9e2cda895e32ecceb4f1c583f
John Peter <jay3pee@gmail.com>
2020-06-18 07:33:38 +0100

    feat: added the handle page for the ministers and ministries

--- js/main.js
+++ js/main.js
@@ -2,11 +2,31 @@ const menu = document.getElementById('menu');
 const nav = document.getElementById('nav');
 const exit = document.getElementById('exit');
 
-menu.addEventListener('click', function(e){
+let minister = document.getElementById('minister-tab');
+let ministry = document.getElementById('ministry-tab');
+
+
+menu.addEventListener('click', function (e) {
     nav.classList.toggle('hide-mobile');
     e.preventDefault()
 })
-exit.addEventListener('click', function(e){
+exit.addEventListener('click', function (e) {
     nav.classList.add('hide-mobile');
     e.preventDefault()
 })
+
+// Togle the display of the Minister and Ministries twitter handles
+function display(option) {
+    if (option == 'minister') {
+        console.log(minister);
+
+        minister.style.display = 'block';
+        ministry.style.display = 'none';
+    } else {
+        console.log(ministry);
+
+        ministry.style.display = 'block';
+        minister.style.display = 'none';
+    }
+
+}
